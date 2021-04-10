3ffafb8fde100afd0a510aa0e3a4e79357145a12
katrinak2021 <79724476+katrinak2021@users.noreply.github.com>
2021-04-10 19:12:41 +0200

    Added 2 tasks as 1st homework
    Added "HelloGroup" as the first task + additional task with user input and Scanner.

---
+++ src/Task1_AdditionalWithName.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task1_AdditionalWithName {
+    public static void main(String[] args){
+        Scanner Name = new Scanner(System.in);
+        String a;
+        System.out.println("Please enter your name");
+        a = Name.next();
+        System.out.println("Hello, " + a);
+
+    }
+
+}
---
+++ src/Task1_HelloGroup.java
@@ -0,0 +1,6 @@
+public class Task1_HelloGroup {
+    public static void main(String[] args){
+        System.out.println("Hello, ");
+        System.out.println("TeachMeSkills group!");
+    }
+}
